170ada89d7a5a424b6d61b1ede7c96b5756cd0f1
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-11-28 15:30:47 +0900

    test : join()에 대한 테스트

---
+++ core/core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join(){
+        //given 이런 환경일 때
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        //when 이렇게 하면
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        //then 요렇게 된다 !
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
