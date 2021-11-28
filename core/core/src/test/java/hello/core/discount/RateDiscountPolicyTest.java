6a03bb5ed0d98bcc1a582d4606818c59e785b35f
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-11-28 19:11:32 +0900

    refactor : 할인 적용이 되어선 안될 때 테스트 추가

--- core/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ core/core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -21,4 +21,16 @@ class RateDiscountPolicyTest {
         //then
         Assertions.assertThat(discount).isEqualTo(1000);
     }
+    
+    
+    @Test
+    @DisplayName("VIP가 아니면 10%할인이 적용되어선 안된다.")
+    void vip_x(){
+        //given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member,10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000); // 1000이 아닌 0이여야함.
+    }
 }
