bf78670538395440d6700d8aceeae39a2f6cbeec
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-11-30 18:24:42 +0900

    refactor : AppConfig 사용하도록 수정

--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,7 @@ import hello.core.member.MemberServiceImpl;
 public class MemberApp {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); // appConfig를 통해서 실행, memberService 객체는 memberServiceImpl 이 들어가 있다. AppConfig 에서 생성자로 생성되어있기 때문
+        MemberService memberService = appConfig.memberService(); // appConfig를 통해서 실행 , memberService 객체는 memberServiceImpl 이 들어가 있다. AppConfig 에서 생성자로 생성되어있기 때문
 //        MemberService memberService = new MemberServiceImpl();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
