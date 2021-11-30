c4787315e73a95015e119a65ae949d207cfbce1b
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-11-30 17:43:47 +0900

    refactor : DIP 원칙에 맞게 수정 (생성자 통한 의존성 주입)

--- core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,13 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    
+    //private final MemberRepository memberRepository = new MemoryMemberRepository(); // 문제점 : 추상화와 구체화에 모두 의존하고 있다 -> DIP 위배
+    private final MemberRepository memberRepository; // 인터페이스에만 의존하고 있으므로 추상화에만 의존하고 있다.
+
+    public MemberServiceImpl(MemberRepository memberRepository) { // 생성자를 통한 의존성 주입
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
