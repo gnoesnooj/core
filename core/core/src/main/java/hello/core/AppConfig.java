046e66473d440b9b592334c040f6ad0735dd5fad
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-11-30 22:41:12 +0900

    refactor : 스프링 컨테이너 사용

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -2,28 +2,37 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration // 애플리케이션 설정정보, 구성정보
 public class AppConfig { //
 
+    @Bean // bean을 통해 스프링 컨테이너에 등록
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository(){
+    @Bean
+    public MemberRepository memberRepository(){
         return new MemoryMemberRepository(); // 중복 제거를 통해 수정시 이부분만 건드리면 된다.
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
