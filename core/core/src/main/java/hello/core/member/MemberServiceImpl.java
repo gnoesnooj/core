f6cae0e9b3683b8dca8b0f9e32e5195739bbd378
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-12-05 20:24:52 +0900

    refactor : 컴포넌트 스캔과 의존관계 자동 주입

--- core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
     //private final MemberRepository memberRepository = new MemoryMemberRepository(); // 문제점 : 추상화와 구체화에 모두 의존하고 있다 -> DIP 위배
     private final MemberRepository memberRepository; // 인터페이스에만 의존하고 있으므로 추상화에만 의존하고 있다.
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) { // 생성자를 통한 의존성 주입
         this.memberRepository = memberRepository;
     }
