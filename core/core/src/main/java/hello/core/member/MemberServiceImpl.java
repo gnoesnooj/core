af7dddd49e47c5fe7fb0420e805bda6a73f1116f
gnoesnooj <91658692+gnoesnooj@users.noreply.github.com>
2021-12-05 16:46:27 +0900

    feat : @Configuration과 싱글톤 비교를 위한 메소드 추가

--- core/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
-    
     //private final MemberRepository memberRepository = new MemoryMemberRepository(); // 문제점 : 추상화와 구체화에 모두 의존하고 있다 -> DIP 위배
     private final MemberRepository memberRepository; // 인터페이스에만 의존하고 있으므로 추상화에만 의존하고 있다.
 
@@ -17,5 +16,9 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
     }
 }
